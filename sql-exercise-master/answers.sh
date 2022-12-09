093cde7b788a99a3b693413d2dcbb511d7d3d09d
Mahmoud <mahmoudkalekish18@gmail.com>
2022-12-09 10:06:09 +0200

    first commit

---
+++ sql-exercise-master/answers.sh
@@ -0,0 +1,7 @@
+Q1: SELECT Name FROM students;
+Q2: SELECT * FROM students WHERE Age>30;
+Q3: SELECT Name FROM students WHERE Age=30 AND Gender='F';
+Q4: SELECT Points FROM students WHERE Name='Alex';
+Q5: INSERT INTO students VALUES ('Mahmoud', 22, 'M', 650);
+Q6: UPDATE students SET Points = Points + 50 WHERE Name='Basma';
+Q7: UPDATE students SET Points = Points - 150 WHERE Name='Alex';
